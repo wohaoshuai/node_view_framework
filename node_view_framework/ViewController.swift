778f453eece75a5bb2d38863abe52ca349e17750
Kelong Wu <kwu18@illinois.edu>
2017-02-14 17:35:35 -0600

    Initial Commit

---
+++ node_view_framework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  node_view_framework
+//
+//  Created by Kelong Wu on 2/14/17.
+//  Copyright © 2017 alien_robot_cat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
